ea7971cbb747cd14b3449e70a0d008310c87dd02
Ashish Sharma <ashishhariom14@gmail.com>
2026-08-02 10:58:34 +0530

    added admin api

---
+++ src/main/java/com/ash/webapp/controller/AdminController.java
@@ -0,0 +1,37 @@
+package com.ash.webapp.controller;
+
+import java.util.List;
+
+import com.ash.webapp.model.User;
+import com.ash.webapp.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+//@CrossOrigin(origins = "http://localhost:5173")
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+    @Autowired
+    public AdminService adminService;
+
+    @GetMapping("/blocked")
+    public ResponseEntity<List<User>> getBlockedUsers() {
+        List<User> blockedUsers = adminService.getBlockedUsers();
+        return ResponseEntity.ok(blockedUsers);
+    }
+
+    @PutMapping("/unblock/{userId}")
+    public ResponseEntity<String> unblockUser(@PathVariable int userId){
+        adminService.unblockUser(userId);
+        return ResponseEntity.ok("User unblocked successfully!");
+    }
+
+}
